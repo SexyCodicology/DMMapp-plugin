58795252ed7537c3d819e67b790c04043cf19fd6
VUW\mennag <g.menna@library.leidenuniv.nl>
2017-08-08 15:12:51 +0200

    Intial code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sexy Codicology Plugin
+The Sexy Codicology Plugin is a Chrome plugin that queries the DMMapp data and lets you find digitized medieval manuscript repositories on the fly!
---
+++ js/options.js
@@ -0,0 +1,28 @@
+//Google Analytics - Replace **** with your GA id! 
+/*
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '****']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+*/
+
+//SEARCHBOX STUFF
+
+window.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('btnOpenNewTab');
+    var searchstring = $('input[id=searchInput]');
+    link.addEventListener('click', function () {
+        var newURL = 'http://someurlhere.com' + searchstring.val();
+        chrome.tabs.create({
+            url: newURL
+        });
+    });
+});
