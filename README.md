4b05ff4b640a2ef67efc5aa58568e1e2531fb125
Giulio Menna <giulio.menna@gmail.com>
2018-09-07 21:53:43 +0200

    Push to version 1.5.1

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Sexy Codicology Plugin
 The Sexy Codicology Plugin is a Chrome plugin that queries the DMMapp data and lets you find digitized medieval manuscript repositories on the fly!
+
+## Why a plugin?
+We want to make it as easy as possible for researchers to reach digitized repositories. With this objective in mind, we wanted to create a tool that could fit in their workflow and reduce clicks needed to reach digitized manuscripts.
+
+A Chrome Plugin seemed to be the most efficient way to achieve this.
+
+Skip having to navigate to the website first; just click the plugin and type the institution you are searching for. The handy autocomplete will also help you get there faster.
+
+Missing a repository? As always, let us know!
+
+## Is there a difference between the data in the DMMapp's website and this plugin?
+No, it is the same.
+
+## Plugin history
+We had in mind to create this plugin for a while, but we did not have the time to get to it. Recently Giulio came to develop the "Library Search Assistant" for Leiden University Libraries; this gave him the opportunity to learn a couple of tricks, and how Chrome Extensions work. Once that project was completed, he took what he had learned there and used it here.
+
+## Credits
+This plugin uses parts of:
+ * easy-autocomplete - As the name suggests, it helps with the autocomplete part of this plugin.
+ * Bootstrap for Chrome Extensions
+
+## Authors
+* Giulio Menna
+* Marjolein de Vos
+
+# License
+MIT - Have fun with the code!
--- js/options.js
+++ js/options.js
@@ -1,28 +1,28 @@
-//Google Analytics - Replace **** with your GA id! 
-/*
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', '****']);
-_gaq.push(['_trackPageview']);
-
-(function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-*/
-
-//SEARCHBOX STUFF
-
-window.addEventListener('DOMContentLoaded', function () {
-    var link = document.getElementById('btnOpenNewTab');
-    var searchstring = $('input[id=searchInput]');
-    link.addEventListener('click', function () {
-        var newURL = 'http://someurlhere.com' + searchstring.val();
-        chrome.tabs.create({
-            url: newURL
-        });
+$(document).ready(function () {
+    var options = {
+        url: "https://digitizedmedievalmanuscripts.org/app/js/data.json",
+        getValue: function (element) {
+            return $(element).prop("Library") + ", " + $(element).prop("City") + ", " + $(element).prop("Nation");
+        },
+        requestDelay: "250",
+        template: {
+            type: "links",
+            fields: {
+                link: "Website"
+            }
+        },
+        list: {
+            maxNumberOfElements: 250,
+            match: {
+                enabled: true
+            },
+            sort: {
+                enabled: true
+            }
+        }
+    };
+    $("#data-json").easyAutocomplete(options);
+    $("#helpbtn").click(function(){
+        $(".info").toggle("fast");
     });
 });
